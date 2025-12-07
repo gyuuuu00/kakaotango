8de9054a3b059a58c109182130619496e001fb95
gyuuuu00 <gyyyuuu030@gmail.com>
2025-12-07 19:23:57 +0900

    1024px -  heatmap/record 나란히 두기, 너비 맞추기 완 _ detailed은 사이즈 조정 안함,추가 수정 필요

--- src/components/MobileBodyReport/MobileBodyReport.jsx
+++ src/components/MobileBodyReport/MobileBodyReport.jsx
@@ -168,15 +168,15 @@ function MobileBodyReport({ data: initialData, t_r}) {
             pelvisTrajectoryUrl={dynamicMat?.mat_hip_trajectory_image_name}
           />
 
-          <div
-          //  style={{ borderTop: '1px solid #D9D9D9', borderRadius: '32px'}} 
-           >
+          <div>
             <DetailedAnalysis
               detailedAnalysis={data.detail_data}
               summaryData={data.result_summary_data}
             />
-            <Heatmap heatmapData={data} />
-            <Record recordData={data.result_history_data} />
+            <div className={styles.bottomRow}>
+              <Heatmap heatmapData={data} />
+              <Record recordData={data.result_history_data} />
+            </div>
           </div>
         </>
       )}
